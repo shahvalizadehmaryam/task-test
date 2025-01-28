bdba7889e3661e0a14178d3cc7ba4a3be6217ff3
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-28 11:22:26 +0330

    some unnecessary imports deleted from router.jsx

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,7 +1,5 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import RegistrationPage from "pages/RegistrationPage";
 import LoginPage from "pages/LoginPage";
-import NotFoundPage from "pages/NotFound";
 import AuthProvider from "providers/AuthProvider";
 import Home from "../components/templates/Home";
 import { getCookie } from "../utils/cookie";
